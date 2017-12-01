19f83991bc58b528eb631e07184e29104726f52e
hexxend <hexxend@sdf.org>
2017-11-30 22:32:32 -0500

    add parameter for username

--- localsock.sh
+++ localsock.sh
@@ -9,13 +9,14 @@
 
 
 local_port=$1
-host_name=$2
-port=$3
+usrname=$2
+host_name=$3
+port=$4
 
 if [ -z $1 ]; then
     echo -e 'usage: localsock.sh local_port remote_host remote_port\n\nCreates a tunnel to remote_host at remote_port on local_port'
     exit
 else
-    ssh -fND localhost:$local_port  root@$host_name -p $port
+    ssh -fND localhost:$local_port  $usrname@$host_name -p $port
     exit
 fi
